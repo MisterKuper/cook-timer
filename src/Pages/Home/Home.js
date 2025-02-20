9caae94f83ff74130f9f9887a556740b04049912
MisterKuper <102316501+MisterKuper@users.noreply.github.com>
2025-02-20 19:48:47 +0100

    mobile adaptation

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,27 +1,28 @@
-import React from 'react'
-import Header from '../../components/Header/Header.js'
-import Category from '../../components/Category/Category.js'
-import { assets } from '../../assets/assets.js'
-import Footer from '../../components/Footer/Footer.js'
+import React from "react";
+import Header from "../../components/Header/Header.js";
+import Category from "../../components/Category/Category.js";
+import { assets } from "../../assets/assets.js";
+import Footer from "../../components/Footer/Footer.js";
 
 function Home() {
   return (
-    <div className='home-container'
-    style={{
-      backgroundImage: `url(${assets.pattern_sky})`,
-      backgroundRepeat: 'repeat-x',
-      backgroundSize: "contain",
-      backgroundPosition: "center",
-      backgroundAttachment: "fixed",
+    <div
+      className="home-container"
+      style={{
+        backgroundImage: `url(${assets.pattern_sky})`,
+        backgroundRepeat: "repeat-x",
+        backgroundSize: "contain",
+        backgroundPosition: "center",
+        backgroundAttachment: "fixed",
       }}
     >
-      <div className='wrapper'>
+      <div className="wrapper">
         <Header />
         <Category />
         <Footer />
       </div>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
